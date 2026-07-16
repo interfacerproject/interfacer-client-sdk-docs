486b345de36120ea95541d90b543602b6c419ef3
phoebus-84 <ennio@dyne.org>
2026-07-16 11:54:04 +0200

    fix: layout:page→layout:doc on 19 pages, add Next Steps card grid CSS

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,6 @@
 import DefaultTheme from "vitepress/theme";
 import Playground from "../components/Playground.vue";
+import "./custom.css";
 import type { Theme } from "vitepress";
 
 export default {
--- docs/getting-started/quick-start.md
+++ docs/getting-started/quick-start.md
@@ -1,5 +1,5 @@
 ---
-layout: page
+layout: doc
 ---
 
 <script setup>
